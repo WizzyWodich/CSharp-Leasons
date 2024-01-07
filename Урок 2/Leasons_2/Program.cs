26d3916c441b91b22115b45ab94ba9412d8a0f7c
WizzyWodich <wizzywodich@gmail.com>
2024-01-07 21:43:42 +0200

    Added leasons 1-3

---
+++ Урок 2/Leasons_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+// Консольный ввод-вывод
+// Если пишем в Visual Studio или Visual Studio Code то можно написать cw и нажать TAB что бы быстро ввести Console.WriteLine()
+
+namespace Leasons_2
+{
+    // Console.WriteLine - Консольный вывод. 
+    // Console.Write - Работает так же как и Console.WriteLine только без добавления перехода на слудующую строку.
+    // Console.ReadLine - Консольный ввод а точнее консольное считывание строки.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создадим строковую переменную и инициализируем ее.
+            string message = "Hello world";
+            // Что бы вывести содержимое переменной используем:
+            Console.WriteLine(message);
+            // Но мы можем обходиться и без такого варианта в редких случаях.
+            Console.WriteLine("Hello world");
+            // Мы можем сделать объединение (и объединение с переменной)
+            Console.WriteLine("Hello " + "world");
+            Console.WriteLine("C# language " + message);
+            // Работа с переменными объявлю и инициализирую пару переменных.
+            string Human = "Bob", Human_2 = "Alex";
+            /* Допустим я хочу вывести две переменых в одну строку. Есть два метода. */
+            // Метод 1: Использовать интерполяцию строк для этого есть оператор $
+            Console.WriteLine($"{Human} and {Human_2} learn a programming language С#");
+            // Метод 2: Использовать плейсхолдеры {0}
+            Console.WriteLine("{0} and {1} learn a programming language С#", Human, Human_2);
+
+            Console.Write("{0} and {1} learn a programming language С#", Human, Human_2);
+
+            // Консольный ввод или же консольное считывание.
+            // Допустим ввод своего имени и вывод его в консоль.
+            Console.WriteLine("Введите свое имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+        }
+    }
+}
+
---
+++ Урок 3/Leasons_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+// Конвектирование типов данных
+namespace Leasons_3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Объявим пару переменых.
+            string name;
+            int age;
+            decimal salary;
+
+            // Так как в С# все представленно в виде строк появляеться вопрос как ввести возраст и зарплату в их переменные с типами int и decimal?
+            // Для этого нам предеться конвертировать string в int, decimal или другие типы данных (float double ...)
+            // И в C# есть метод конвертирования:
+            // <Convert.><Тип данных .NET>();
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите размер зарплаты: ");
+            salary = Convert.ToDecimal(Console.ReadLine());
+
+            // Выведу в консоль 
+            // Обявлю еще одну переменую.
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Имя: {name}  Возраст: {age} Рост: {height} Зарплата: {salary}");
+        }
+    }
+}
