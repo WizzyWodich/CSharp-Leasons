a372010e658d0e62d60ed84fdcba0459209d7aba
WizzyWodich <wizzywodich@gmail.com>
2024-01-13 01:02:57 +0200

    Added leasons 5-6.3

--- Урок 6.3/Leasons_6.3/obj/Debug/net8.0/Leasons_6.3.AssemblyInfo.cs
+++ Урок 6.3/Leasons_6.3/obj/Debug/net8.0/Leasons_6.3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Leasons_6.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21e1214cba44b4b69c4dd06d9d617bd88627c75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Leasons_6.3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Leasons_6.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
